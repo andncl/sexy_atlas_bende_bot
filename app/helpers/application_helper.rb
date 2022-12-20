ecc37eeadea73017abe8c9cfc6be30e502e40fdc
Andreas Nickl <andreas@ans.app>
2022-12-20 15:20:16 +0100

    Added check_user helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,20 @@
 module ApplicationHelper
-
 	def message_processor(full_message)
 		puts full_message.to_s
+	end
+
+	def check_user()
+	  if Member.find_by_id(from['id'].to_s).exists?
+	    user = Member.find_by(telegram_id: from['id'])
+	  else
+	    user = Member.create(
+	    	id: from['id']
+	      first_name: from['first_name'],
+	      last_name: from['first_name'],
+	      phone_number: 1234567,
+	      balance: 0.0,
+	      telegram_id: from['id']
+	      )
+	  end
 	end
 end
