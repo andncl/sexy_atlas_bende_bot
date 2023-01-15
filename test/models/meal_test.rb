4bfc68487cf5876b515ad2cf5c1f7710e6bf8f85
andi <andreas@orangeqs.com>
2023-01-15 17:49:02 +0100

    Added test for header generation

--- test/models/meal_test.rb
+++ test/models/meal_test.rb
@@ -4,4 +4,18 @@ class MealTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @meal = create(:meal)
+    @user1 = create(:user)
+    @user2 = create(:user)
+  end
+
+  test 'generate_meal_overview' do
+    eater1 = create(:eater, user: @user1, meal: @meal, first_name: 'tobi')
+    eater2 = create(:eater, user: @user2, meal: @meal, first_name: 'jens')
+    @meal.update(cook_id: @user1.id)
+
+    assert_includes @meal.generate_meal_overview, 'is cooking'
+  end
 end
