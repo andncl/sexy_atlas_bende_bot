b3f82bf92aa1f350a5ee66c949f4787736a4c255
andi <andreas@orangeqs.com>
2023-01-22 13:00:55 +0100

    Fixed join/leave conditions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,19 @@ class User < ApplicationRecord
 	has_many :meals
 	has_many :eaters, through: :meals
 
+  def init
+  	self.cooking ||= falses           #will set the default value only if it's nil
+  end
+
 	def is_eater(meal)
-		Eater.where(meal_id: meal[:id]).exists?
+		Eater.exists?(meal: meal, user: self)
+	end
+
+	def is_eating(meal)
+		Eater.find_by(meal: meal, user: self).eating
+	end
+
+	def is_cooking(meal)
+		Eater.find_by(meal: meal, user: self).eating
 	end
 end
